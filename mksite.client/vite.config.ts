1da5f0d238f70a16db13f5362570ebf6f951b9d2
Madeeha Khan <madeehakhan16@gmail.com>
2024-10-20 17:32:59 -0400

    "logging to find the problem"

--- mksite.client/vite.config.ts
+++ mksite.client/vite.config.ts
@@ -17,7 +17,7 @@ const baseFolder =
 const certificateName = "mksite.client";
 const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
 const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
-
+console.log(`base folder: ${baseFolder}`);
 if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
     if (0 !== child_process.spawnSync('dotnet', [
         'dev-certs',
