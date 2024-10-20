11ebae9d89921ccf7348ac03b6f5e812ec181b30
Madeeha Khan <madeehakhan16@gmail.com>
2024-10-20 17:14:05 -0400

    trying vite plugin for cert failure

--- mksite.client/vite.config.ts
+++ mksite.client/vite.config.ts
@@ -6,6 +6,8 @@ import fs from 'fs';
 import path from 'path';
 import child_process from 'child_process';
 import { env } from 'process';
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
 
 const baseFolder =
     env.APPDATA !== undefined && env.APPDATA !== ''
@@ -35,7 +37,7 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [plugin()],
+    plugins: [plugin(), basicSsl()],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
