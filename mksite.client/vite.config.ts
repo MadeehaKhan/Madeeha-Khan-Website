ae09f394681ddfe70270154e88f32cc6d80e4516
Madeeha Khan <madeehakhan16@gmail.com>
2024-10-24 23:47:47 -0400

    Vite config with local settings

--- mksite.client/vite.config.ts
+++ mksite.client/vite.config.ts
@@ -2,10 +2,35 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import plugin from '@vitejs/plugin-react';
+import fs from 'fs';
+import path from 'path';
+import child_process from 'child_process';
 import { env } from 'process';
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
 
+const baseFolder =
+    env.APPDATA !== undefined && env.APPDATA !== ''
+        ? `${env.APPDATA}/ASP.NET/https`
+        : `${env.HOME}/.aspnet/https`;
+console.log(env.HOME)        
+
+const certificateName = "mksite.client";
+const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
+const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
+if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+    if (0 !== child_process.spawnSync('dotnet', [
+        'dev-certs',
+        'https',
+        '--export-path',
+        certFilePath,
+        '--format',
+        'Pem',
+        '--no-password',
+    ], { stdio: 'inherit', }).status) {
+        console.log(new Error().stack);
+    }
+}
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
     env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7012';
@@ -32,6 +57,10 @@ export default defineConfig({
                 secure: false
             }
         },
-        port: 5173
+        port: 5173,
+        https: {
+            key: fs.readFileSync(keyFilePath),
+            cert: fs.readFileSync(certFilePath),
+        }
     }
 })
